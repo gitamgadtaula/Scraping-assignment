602f0b095031e502c78c62e2d55e9de98978ea7c
gitam gadtaula <gitamgadtaula@gmail.com>
2022-11-13 13:27:58 +0545

    scraping 5 nepali sites to predict the election outcome

---
+++ results.py
@@ -0,0 +1,23 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+from collections import Counter
+import matplotlib.pyplot as plt
+f = open('stopwords01.txt', 'r')  # Importing stopwords
+stop_text = f.read()
+tokenize_words = word_tokenize(stop_text)
+list = open("corpus.txt", "r").read().split()
+word_dict = Counter(list)
+c = {k: v for k, v in word_dict.items() if k not in tokenize_words}
+counts = Counter(c).most_common()[:20]
+fig = plt.figure(figsize=(10, 5))
+x_axis, y_axis = [], []
+
+
+def print_graph(top_count):
+    for each in top_count:
+        x_axis.append(each[0])
+        y_axis.append(each[1])
+    plt.bar(x_axis, y_axis, color='maroon', width=0.4)
+    plt.show()
+
+
+print_graph(counts)
---
+++ scrape.py
@@ -0,0 +1,44 @@
+from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+urls = ['https://election.onlinekhabar.com',
+        'https://setopati.com',
+        'https://ekantipur.com',
+        'https://ratopati.com',
+        'https://nagariknews.nagariknetwork.com'
+        ]
+
+f = open('corpus.txt', 'a')  # A file where we store our scraped data
+
+
+for url in urls:
+    # running loop on our targeted newspaper one by one
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    cleanedUrls = []
+    for link in soup.find_all('a'):
+        if "https://" in link['href']:
+            cleanedUrls.append(link["href"])
+            print('___________________________________________')
+            print('scraping ....', link.get('href'))
+            article = Article(link.get('href'), language='hi')
+            article.download()
+            # article.html
+            article.parse()
+            article.nlp()
+            data = {
+                "TITLE": article.title,
+                "keywords": article.keywords,
+                "summary": article.summary,
+                "TEXT": article.text,
+                "img_url": article.top_image,
+            }
+            f.write(article.title)
+            f.write(article.text)
+            print(data)
+    # print(article.keywords)
+f.close()
